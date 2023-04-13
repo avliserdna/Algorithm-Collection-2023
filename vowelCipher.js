44a9082280cf595eafaf1a435cabb467817faf65
Andre <andre.christian.silva@gmail.com>
2023-04-13 16:58:18 -0700

    Sovled vowelCipher.js

--- vowelCipher.js
+++ vowelCipher.js
@@ -1,5 +1,29 @@
 function vowelCipher(string) {
-    // your code here
+    // have a const string of vowels "aeiou"
+    // check and see if letter in character IS in string of vowels
+    // IF its in string of vowels, return the index location +1 UNLESS it's past 5. If its past 5, return 0
+
+    const vowels = "aeiou";
+    const vowelsArr = vowels.split("")
+
+    const stringArr = string.split("")
+    for (let i = 0; i < stringArr.length; i++) {
+        if (vowelsArr.includes(stringArr[i])) {
+            stringArr[i] = vowelConversion(stringArr[i], vowels)
+        }
+    }
+    return stringArr.join("")
+}
+
+function vowelConversion(char, vowels) {
+    let charIndex = vowels.indexOf(char)
+    if (charIndex + 1 >= vowels.length) {
+        charIndex = 0;
+    }
+    else {
+        charIndex += 1
+    }
+    return vowels[charIndex]
 }
 
 
