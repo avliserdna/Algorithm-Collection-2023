b27835fbc3a21f71141cc6ba259593ca2abcac2d
Andre <andre.christian.silva@gmail.com>
2023-04-13 16:45:08 -0700

    Fixed file names. vowelCipher is now caesarCipher. New problem for vowelCipher.

---
+++ caeserCipher.js
@@ -0,0 +1,31 @@
+// Feel free to use this variable:
+// const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+function caesarCipher(string, num) {
+
+    // iterate through string
+    // look for where char of string IS in alphabet
+    // add by num and see where it ends up on the alphabet
+    // if index number hits ABOVE alphabet's  length, reset to 0 and add there
+    const stringArr = string.split("")
+
+    for (let i = 0; i < stringArr.length; i++) {
+        stringArr[i] = cipher(stringArr[i], num)
+    }
+    return stringArr.join("")
+}
+
+function cipher(char, num) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let charIndex = alphabet.indexOf(char)
+    let newIndex = charIndex + num;
+    if (newIndex > alphabet.length) {
+        newIndex = newIndex - alphabet.length
+    }
+    return alphabet[newIndex]
+}
+
+console.log(caesarCipher("apple", 1)); // "bqqmf"
+console.log(caesarCipher("bootcamp", 2)); // "dqqvecor"
+console.log(caesarCipher("zebra", 4)); // "difve"
+console.log(caesarCipher("xylophone", 3)); // "borskrqh"
--- vowelCipher.js
+++ vowelCipher.js
@@ -1,31 +1,7 @@
-// Feel free to use this variable:
-// const alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-function caesarCipher(string, num) {
-
-    // iterate through string
-    // look for where char of string IS in alphabet
-    // add by num and see where it ends up on the alphabet
-    // if index number hits ABOVE alphabet's  length, reset to 0 and add there
-    const stringArr = string.split("")
-
-    for (let i = 0; i < stringArr.length; i++) {
-        stringArr[i] = cipher(stringArr[i], num)
-    }
-    return stringArr.join("")
+function vowelCipher(string) {
+    // your code here
 }
 
-function cipher(char, num) {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz";
-    let charIndex = alphabet.indexOf(char)
-    let newIndex = charIndex + num;
-    if (newIndex > alphabet.length) {
-        newIndex = newIndex - alphabet.length
-    }
-    return alphabet[newIndex]
-}
 
-console.log(caesarCipher("apple", 1)); // "bqqmf"
-console.log(caesarCipher("bootcamp", 2)); // "dqqvecor"
-console.log(caesarCipher("zebra", 4)); // "difve"
-console.log(caesarCipher("xylophone", 3)); // "aborkrqh"
+console.log(vowelCipher("bootcamp")); // "buutcemp"
+console.log(vowelCipher("paper cup")); // "pepir cap"
