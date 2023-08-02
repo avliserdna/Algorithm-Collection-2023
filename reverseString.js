fe30cc9a47c48384636353994b6d06d054b5deb1
avliserdna <andre.christian.silva@gmail.com>
2023-08-01 22:15:28 -0700

    solved reversed string with a slightly more optimal solution.

--- reverseString.js
+++ reverseString.js
@@ -1,5 +1,12 @@
-let reverseString = (string) => string.split("").reverse().join("")
+// let reverseString = (string) => string.split("").reverse().join("")
 
+function reverseString(string) {
+  let reversed = ''
+  for (let i = string.length-1; i >= 0; i--) {
+    reversed += string[i]
+  }
+  return reversed
+}
 
 
 console.log(reverseString('fish')); // 'hsif'
