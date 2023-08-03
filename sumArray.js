208d6bcc56e2379ee0543d931be3d85a6901049b
avliserdna <andre.christian.silva@gmail.com>
2023-08-02 21:07:24 -0700

    finished sumArray with a reducer solution.

--- sumArray.js
+++ sumArray.js
@@ -1,10 +1,11 @@
-function sumArray(array) {
-  let res = 0;
-  for (let i = 0; i < array.length; i++) {
-    res += array[i]
-  }
-  return res
-}
+// function sumArray(array) {
+//   let res = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     res += array[i]
+//   }
+//   return res
+// }
 
+let sumArray = (array) => array.reduce((a,b) => a + b)
 console.log(sumArray([5, 6, 4])); // => 15
 console.log(sumArray([7, 3, 9, 11])); // => 30
