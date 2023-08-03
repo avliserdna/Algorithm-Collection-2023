e1bae55e15b88c59aba0ab353445cb2c2c21a3ac
avliserdna <andre.christian.silva@gmail.com>
2023-08-02 21:01:29 -0700

    finished myIncludes with a one line solution.

---
+++ myIncludes.js
@@ -0,0 +1,13 @@
+let myIncludes = (arr, char) => arr.filter((el) => el === char).length === 1
+
+
+
+
+
+
+
+console.log(myIncludes(['a', 'b', 'c', 'e'], 'c')); // true
+console.log(myIncludes(['a', 'b', 'c', 'e'], 'a')); // true
+console.log(myIncludes(['a', 'b', 'c', 'e'], 'z')); // false
+console.log(myIncludes([43, -7, 11, 13], 11)); // true
+console.log(myIncludes([43, -7, 11, 13], 1)); // false
