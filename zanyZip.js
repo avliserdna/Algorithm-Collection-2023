afec4be4ff7fa3114556065b389a362b972d1fa4
avliserdna <andre.christian.silva@gmail.com>
2023-08-15 14:04:29 -0700

    Made a simpler, and cleaner solution for zanyZip.

--- zanyZip.js
+++ zanyZip.js
@@ -2,36 +2,53 @@
 // same as Zip, but if one array is longer, use the longer array as the iterator's length.
 // check if i+1 is GREATER than smaller array, and if it is assign null on longer the place where shorter array is
 
+
 function zanyZip(arr1, arr2) {
-  let longer = 0;
-  let shorter = 0;
   let result = []
 
-  if (arr1.length > arr2.length) {
-    longer = arr1.length
-    shorter = arr2.length
-  }
-  else if (arr1.length < arr2.length){
-    longer = arr2.length
-    shorter = arr1.length
-  }
-  else {
-    // Both are the same length
-    longer = arr1.length
-    shorter = arr1.length
-  }
-  for (let i = 0; i < longer; i++) {
-    if ((i+1) > shorter) {
-       arr1.length < arr2.length ? result.push([null, arr2[i]]) : result.push([arr1[i], null])
-    }
-    else {
-      result.push([arr1[i], arr2[i]])
+  for (let i = 0; i < arr1.length || i < arr2.length; i++) {
+    if (!arr1[i]) {
+      arr1[i] = null
     }
 
+    if (!arr2[i]) {
+      arr2[i] = null
+    }
 
+    result[i]= [arr1[i], arr2[i]]
   }
   return result
 }
+// function zanyZip(arr1, arr2) {
+//   let longer = 0;
+//   let shorter = 0;
+//   let result = []
+
+//   if (arr1.length > arr2.length) {
+//     longer = arr1.length
+//     shorter = arr2.length
+//   }
+//   else if (arr1.length < arr2.length){
+//     longer = arr2.length
+//     shorter = arr1.length
+//   }
+//   else {
+//     // Both are the same length
+//     longer = arr1.length
+//     shorter = arr1.length
+//   }
+//   for (let i = 0; i < longer; i++) {
+//     if ((i+1) > shorter) {
+//        arr1.length < arr2.length ? result.push([null, arr2[i]]) : result.push([arr1[i], null])
+//     }
+//     else {
+//       result.push([arr1[i], arr2[i]])
+//     }
+
+
+//   }
+//   return result
+// }
 
 
 
