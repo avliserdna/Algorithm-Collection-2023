aef7d9bf9f3b3adb22c49dd9dbb864c745934fb7
avliserdna <andre.christian.silva@gmail.com>
2023-08-14 17:21:16 -0700

    uncompress.js

---
+++ uncompress.js
@@ -0,0 +1,36 @@
+// your code here
+// obtain a string, and split it.
+// Check and see if Number, if not number assign to char
+// if number, assign to number,
+// use number to iterate the char onto the results string
+
+function uncompress(str) {
+  const strArr = str.split("")
+  let char = "";
+  let num = 0;
+  let result = "";
+    for (let i = 0; i < strArr.length; i++) {
+    if (Number(strArr[i])) {
+      num = strArr[i]
+    }
+    else {
+      char = strArr[i]
+    }
+
+    if (char && num) {
+      for (let j = 0; j < num; j++) {
+        result+= char
+      }
+      char = ""
+      num = 0;
+    }
+  }
+  return result
+}
+
+
+
+
+console.log(uncompress('x3y4z2')); // 'xxxyyyyzz'
+console.log(uncompress('a5b2c4z1')); // 'aaaaabbccccz'
+console.log(uncompress('b1o2t1')); // 'boot'
