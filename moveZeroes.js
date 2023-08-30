052e3ef70f4ef8b88f3c4200389f4bae87f99805
avliserdna <andre.christian.silva@gmail.com>
2023-08-30 16:50:43 -0700

    done move zeroes with editorial solution 2 in JS.

--- moveZeroes.js
+++ moveZeroes.js
@@ -3,25 +3,45 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-  // get array of nums
-  // check if current index is 0
-  // if index is 0, swap index IF next index is NOT 0
-  // if both indexes are 0, look for the next index and see if its 0
-  // do all swaps until all 0s are at the end
+  //Count the zeroes
+  let lastNonZero =0
+     // all non-zero elements retain original order
+     for (let i = 0; i< nums.length; i++) {
+       if (nums[i]) {
+        nums[lastNonZero] = nums[i]
+        lastNonZero++
+       }
+       }
+       for (let i = lastNonZero; i < nums.length; i++) {
+         nums[i] = 0;
+       }
+     return nums
 
-  for (let i = 0; i < nums.length-1; i++) {
-      if (!nums[i]) {
-        j=i+1
-             while (!nums[j] && j < nums.length-1) {
-                  j+=1
-              }
-              nums[i] = nums[j]
-              nums[j] = 0
-      }
-
-  }
-      return nums
 };
+// /**
+//  * @param {number[]} nums
+//  * @return {void} Do not return anything, modify nums in-place instead.
+//  */
+// var moveZeroes = function(nums) {
+//   // get array of nums
+//   // check if current index is 0
+//   // if index is 0, swap index IF next index is NOT 0
+//   // if both indexes are 0, look for the next index and see if its 0
+//   // do all swaps until all 0s are at the end
 
-console.log(moveZeroes([0,1,0,3,12]))
-console.log([0])
+//   for (let i = 0; i < nums.length-1; i++) {
+//       if (!nums[i]) {
+//         j=i+1
+//              while (!nums[j] && j < nums.length-1) {
+//                   j+=1
+//               }
+//               nums[i] = nums[j]
+//               nums[j] = 0
+//       }
+
+//   }
+//       return nums
+// };
+
+// console.log(moveZeroes([0,1,0,3,12]))
+// console.log([0])
